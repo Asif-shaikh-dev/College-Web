b8f474f00c98929f551c8099af00c74c14a0c3cc
Asif-shaikh-dev <asifsk.5070@gmail.com>
2025-06-05 02:44:17 +0530

    Update CORS configuration to allow production frontend URL

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.set("trust proxy", 1);
 
 
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:5174'], // Your frontend URL
+    origin: ['https://college-web-frontend-2.onrender.com','http://localhost:5174'], // Your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
